f1ed60ba9f58e3f6c897d7d7c8c235e72a5a2822
krupet <krutsiuk.petr@gmail.com>
2015-06-23 18:36:31 +0300

    basic dbcp added without pooling tuning

--- src/main/java/ua/krupet/DuplexStatisticsHandler.java
+++ src/main/java/ua/krupet/DuplexStatisticsHandler.java
@@ -13,6 +13,7 @@ import io.netty.util.CharsetUtil;
 import io.netty.util.concurrent.GlobalEventExecutor;
 import org.apache.commons.validator.routines.UrlValidator;
 
+import javax.sql.DataSource;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.sql.*;
@@ -50,13 +51,27 @@ public class DuplexStatisticsHandler extends ChannelDuplexHandler {
     private HttpRequest httpRequest;
     private final StringBuilder buf = new StringBuilder();
 
-    private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
-    private static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/nettydb";
-    private static final String DB_USER = "javamanager";
-    private static final String DB_PASSWORD = "javamanager";
+//    private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+//    private static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/nettydb";
+//    private static final String DB_USER = "javamanager";
+//    private static final String DB_PASSWORD = "javamanager";
 
     private String requestIp;
     private Timestamp requestTime;
+
+    private DataSource dataSource;
+
+    public DuplexStatisticsHandler() {
+    }
+
+    public DuplexStatisticsHandler(DataSource dataSource) {
+        super();
+        this.dataSource = dataSource;
+    }
+
+    private Connection getDBConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
@@ -381,28 +396,28 @@ public class DuplexStatisticsHandler extends ChannelDuplexHandler {
         }
     }
 
-    private static Connection getDBConnection() {
-        Connection dbConnection = null;
-
-        try {
-
-            Class.forName(DB_DRIVER);
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            dbConnection = DriverManager.getConnection(
-                    DB_CONNECTION, DB_USER, DB_PASSWORD);
-            return dbConnection;
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return dbConnection;
-    }
+//    private static Connection getDBConnection() {
+//        Connection dbConnection = null;
+//
+//        try {
+//
+//            Class.forName(DB_DRIVER);
+//
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//
+//        try {
+//            dbConnection = DriverManager.getConnection(
+//                    DB_CONNECTION, DB_USER, DB_PASSWORD);
+//            return dbConnection;
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return dbConnection;
+//    }
 
     private long getTotalAmountRequest(String query) {
         long totalAmount = 0L;
--- src/main/java/ua/krupet/HttpNettyServerInitializer.java
+++ src/main/java/ua/krupet/HttpNettyServerInitializer.java
@@ -7,11 +7,21 @@ import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpRequestDecoder;
 import io.netty.handler.codec.http.HttpResponseEncoder;
 import io.netty.handler.traffic.ChannelTrafficShapingHandler;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
 
 /**
  * Created by krupet on 20.04.2015.
  */
 public class HttpNettyServerInitializer extends ChannelInitializer<SocketChannel> {
+
+
+    private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/nettydb";
+    private static final String DB_USER = "javadev";
+    private static final String DB_PASSWORD = "javadev";
+
     @Override
     public void initChannel(SocketChannel ch) {
         ChannelPipeline p = ch.pipeline();
@@ -20,7 +30,19 @@ public class HttpNettyServerInitializer extends ChannelInitializer<SocketChannel
         p.addLast("decoder", new HttpRequestDecoder());
         p.addLast(new HttpObjectAggregator(1048576));
         p.addLast("encoder", new HttpResponseEncoder());
-        p.addLast("duplex statistic handler",new DuplexStatisticsHandler());
+        p.addLast("duplex statistic handler",new DuplexStatisticsHandler(setupDataSource()));
+//        p.addLast("duplex statistic handler",new DuplexStatisticsHandler());
         p.addLast("request handler",new RequestHandler());
+    }
+
+    public static DataSource setupDataSource() {
+
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName(DB_DRIVER);
+        ds.setUrl(DB_CONNECTION);
+        ds.setUsername(DB_USER);
+        ds.setPassword(DB_PASSWORD);
+
+        return ds;
     }
 }
